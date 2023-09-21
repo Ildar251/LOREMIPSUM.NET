ed943f5570e432a1af1f2ea480b84963abbf6016
ildar <ildar251>
2023-09-21 22:46:52 +0300

    deploy

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 window.onload = function() { 
     document.body.classList.add('loaded_hiding')
+    document.html.classList.add('hidden')
     window.setTimeout(function () {
         document.body.classList.add('loaded')
         document.body.classList.remove('loaded_hiding')
+        document.html.classList.remove('hidden')
     }, 500)
 
-    
+
     const menu = document.querySelector("ul.menu");
     document.querySelector(".burgerMenu-checkbox").onchange = function(){
         menu.classList.toggle("menu_active");
